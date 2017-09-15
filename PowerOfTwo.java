6a17c1216033462781b4fd13ae403d75ad104022
Aditya Mahlawat <adi.mahlawat@gmail.com>
2017-09-15 13:14:15 +0530

    Update PowerOfTwo.java

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -8,5 +8,11 @@ boolean PowerOfTwo(int x)
             while(x % 2 == 0) x /= 2;
             return (x == 1);
             }
+    //Another method to reduce time complexity
+/*    boolean isPowerOfTwo(int x)
+    {
+        // x will check if x == 0 and !(x & (x - 1)) will check if x is a power of 2 or not
+        return (x && !(x & (x - 1)));
+    }*/
     }
     
